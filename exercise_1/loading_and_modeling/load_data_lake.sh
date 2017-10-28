8456ac6a949ca2c09434368cf4993a0902438115
rayleegit <w205@ip-172-31-87-9.ec2.internal>
2017-10-28 23:48:03 +0000

    updated load_data_lake.sh

--- exercise_1/loading_and_modeling/load_data_lake.sh
+++ exercise_1/loading_and_modeling/load_data_lake.sh
@@ -27,7 +27,7 @@ NEW_FILE3="readmissions.csv"
 OLD_FILE4="Measure Dates.csv"
 NEW_FILE4="measures.csv"
 OLD_FILE5="hvbp_hcahps_11_10_2016.csv"
-NEW_FILE5="surveys_responses.csv"
+NEW_FILE5="survey_responses.csv"
 
 tail -n +2 "$OLD_FILE1" > $NEW_FILE1
 tail -n +2 "$OLD_FILE2" > $NEW_FILE2
@@ -35,15 +35,20 @@ tail -n +2 "$OLD_FILE3" > $NEW_FILE3
 tail -n +2 "$OLD_FILE4" > $NEW_FILE4
 tail -n +2 "$OLD_FILE5" > $NEW_FILE5
 
-# create our hdfs directory
+# create our hdfs directories
 hdfs dfs -mkdir /user/w205/hospital_compare
+hdfs dfs -mkdir /user/w205/hospital_compare/hospitals
+hdfs dfs -mkdir /user/w205/hospital_compare/effective_care
+hdfs dfs -mkdir /user/w205/hospital_compare/readmissions
+hdfs dfs -mkdir /user/w205/hospital_compare/measures
+hdfs dfs -mkdir /user/w205/hospital_compare/survey_responses
 
-# copy the files to hdfs
-hdfs dfs -put $NEW_FILE1 /user/w205/hospital_compare
-hdfs dfs -put $NEW_FILE2 /user/w205/hospital_compare
-hdfs dfs -put $NEW_FILE3 /user/w205/hospital_compare
-hdfs dfs -put $NEW_FILE4 /user/w205/hospital_compare
-hdfs dfs -put $NEW_FILE5 /user/w205/hospital_compare
+# copy the files to hdfs directories
+hdfs dfs -put $NEW_FILE1 /user/w205/hospital_compare/hospitals
+hdfs dfs -put $NEW_FILE2 /user/w205/hospital_compare/effective_care
+hdfs dfs -put $NEW_FILE3 /user/w205/hospital_compare/readmissions
+hdfs dfs -put $NEW_FILE4 /user/w205/hospital_compare/measures
+hdfs dfs -put $NEW_FILE5 /user/w205/hospital_compare/survey_responses
 
 # change directory back to the original
 cd $MY_CWD
